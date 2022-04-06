890fa2ce9c26aefbf0bf627547d10e6d135c088d
xutianyu <hhuxty0720@163.com>
2022-04-06 21:24:24 +0800

    add tensor2img_fast function

--- utils/img_util.py
+++ utils/img_util.py
@@ -29,3 +29,21 @@ def img2tensor(imgs, bgr2rgb=True, float32=True):
         return [_totensor(img, bgr2rgb, float32) for img in imgs]
     else:
         return _totensor(imgs, bgr2rgb, float32)
+
+
+def tensor2img_fast(tensor, rgb2bgr=True, min_max=(0,1)):
+    """This implementation is slightly faster than tensor2img.
+    It now only support torch tensor with shape (1, c, h, w).
+
+    Args:
+        tensor (Tensor): Now only support torch tensor with (1, c, h, w).
+        rgb2bgr (bool): Whether to change rgb to bgr. Default: True.
+        min_max (tuple[int]): min and max values for clamp.
+    """
+    
+    output = tensor.squeeze(0).detach().clamp_(*min_max).permute(1, 2, 0)
+    output = (output - min_max[0]) / (min_max[1] - min_max[0]) * 255
+    output = output.type(torch.uint8).cpu().numpy()
+    if rgb2bgr:
+        output = cv2.cvtColor(output, cv2.COLOR_RGB2BGR)
+    return output
